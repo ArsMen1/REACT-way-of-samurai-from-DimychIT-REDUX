a3cce555462a14329140d458f3379525934c34e0
OsipovArsen <arsenij-osipov@mail.ru>
2022-04-02 15:08:43 +0300

    75+

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+const LoginForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field name={"login"} placeholder={"login"} component={"input"} />
+      </div>
+      <div>
+        <Field name={"password"} placeholder={"Password"} component={"input"} />
+      </div>
+      <div>
+        <Field name={"rememberMe"} type={"checkbox"} component={"input"} />{" "}
+        Запомнить меня
+      </div>
+      <button>Войти</button>
+    </form>
+  );
+};
+
+const LoginREduxForm = reduxForm({
+  form: "login", // уникальное имя формы
+})(LoginForm);
 
 const Login = (props) => {
-  return <div>Login</div>;
+  const onSubmit = (formData) => {
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginREduxForm onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default Login;
