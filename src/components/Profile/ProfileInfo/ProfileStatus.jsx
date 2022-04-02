cc2fd729cd621f0685de2a604f9238785c3d0ebe
OsipovArsen <arsenij-osipov@mail.ru>
2022-04-02 13:52:47 +0300

    74+

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -20,6 +20,14 @@ class ProfileStatus extends React.Component {
     this.setState({ status: e.currentTarget.value });
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({
+        status: this.props.status,
+      });
+    }
+  }
+
   render() {
     return (
       <>
