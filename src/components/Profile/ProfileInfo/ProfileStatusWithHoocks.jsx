e27fbe8aedc91e06d6b097bc26d9ac5e739c653f
OsipovArsen <arsenij-osipov@mail.ru>
2022-04-05 22:37:24 +0300

    85 useEffect +

--- src/components/Profile/ProfileInfo/ProfileStatusWithHoocks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHoocks.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import s from "./ProfileInfo.module.css";
 
 const ProfileStatusWithHoocks = (props) => {
   let [editMode, setEditMode] = useState(false);
   let [status, setStatus] = useState(props.status);
+
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
 
   const activateMode = () => {
     setEditMode(true);
